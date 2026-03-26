a78b1453076a58b2b361e2ea45828acadb6ada6c
Valor <15707220966@163.com>
2026-03-26 14:40:08 +0800

    更新依赖和 Vite 配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  esbuild: {
+    target: 'es2018',
+  } as any,
+  build: {
+    target: 'es2018',
+  },
+  server: {
+    host: true,
+  },
+  preview: {
+    host: true,
+  },
 })
